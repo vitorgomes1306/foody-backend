0f13d6de16d983ec02a0f65072246144d570908a
Vitor Gomes <vitoralm0613@gmail.com>
2026-03-27 18:46:11 -0300

    rota de teste no backend

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 
 const port = process.env.PORT || 9800;
 
+// rota de teste
+app.get('/test', (req, res) => {
+  res.send('Backend do Foody está rodando!');
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
