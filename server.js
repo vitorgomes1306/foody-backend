be72924ad12270788808a9b80007c336910eb8ad
Vitor Gomes <vitoralm0613@gmail.com>
2026-03-28 00:37:51 -0300

    Alteracoes no mode default

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Foody API online 🚀');
-});
-
 app.get('/test', (req, res) => {
   res.send('Backend do Foody está rodando!');
 });
