ab4a04b6760cca69ff10c7e7f49a5d521a8992e4
Vitor Gomes <vitoralm0613@gmail.com>
2026-03-28 01:00:15 -0300

    rota de cadastro corrigido 3

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+import registerRoutes from './routes/register.js';
+
 app.use(express.json());
 app.use('/api', registerRoutes);
 
@@ -10,9 +12,6 @@ const port = process.env.PORT || 3000;
 app.get('/test', (req, res) => {
   res.send('Backend do Foody está rodando!');
 });
-
-// importar rotas
-import registerRoutes from './routes/register.js';
 
 
 app.listen(port, '0.0.0.0', () => {
